b86283306d6ef923d0c76dde1e665dfebce29dbd
davidquintr <davidquintr97@gmail.com>
2023-06-21 22:35:22 -0600

    si

---
+++ Assets/Samples/Google Cardboard XR Plugin for Unity/1.21.0/Hello Cardboard/Scripts/BasicCameraMovement.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+
+public class DragCamera : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public float sensitivity = 5.0f;
+
+    void Update()
+    {
+        // Move the camera forward, backward, left, and right
+        transform.position += transform.forward * Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        transform.position += transform.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+        // Rotate the camera based on the mouse movement
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+        transform.eulerAngles += new Vector3(-mouseY * sensitivity, mouseX * sensitivity, 0);
+    }
+
+
+}
