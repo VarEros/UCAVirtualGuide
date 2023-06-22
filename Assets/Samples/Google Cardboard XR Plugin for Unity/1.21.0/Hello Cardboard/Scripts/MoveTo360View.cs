9323e93d9733f064d1eb202230606b84c8311413
ErOs <104714204+VarEros@users.noreply.github.com>
2023-06-22 03:20:32 -0600

    ya me teletransportooooooo

--- Assets/Samples/Google Cardboard XR Plugin for Unity/1.21.0/Hello Cardboard/Scripts/MoveTo360View.cs
+++ Assets/Samples/Google Cardboard XR Plugin for Unity/1.21.0/Hello Cardboard/Scripts/MoveTo360View.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using UnityEngine;
 
 public class MoveTo360View : MonoBehaviour
@@ -7,22 +8,52 @@ public class MoveTo360View : MonoBehaviour
     /// <summary>
     /// The material to use when this object is inactive (not being gazed at).
     /// </summary>
-    public Vector3 translation = new Vector3(20f, 2f, 0f);
-    public GameObject target;
-    private Renderer _myRenderer;
+    [SerializeField] private GameObject target;
+    [SerializeField] private Vector3 teleportPose;
+    private MeshRenderer _myRenderer;
+    //private Vector3 teleportPose;
+
+    private bool colorChaning = false;
+    private float myTimer = 0f;
 
     // Start is called before the first frame update
     public void Start()
     {
-        _myRenderer = GetComponent<Renderer>();
+        _myRenderer = GetComponent<MeshRenderer>();
+        //teleportPose = new Vector3(sphere.transform.position.x, sphere.transform.position.y, sphere.transform.position.z)
+    }
+
+    public void Update()
+    {
+        if (colorChaning) {
+            myTimer += Time.deltaTime;
+            if (myTimer >= 2f)
+            {
+                target.transform.position = teleportPose;
+            }
+        }
     }
 
     public void OnPointerEnter()
     {
-        target.transform.position = translation;
+        GazeAt(true);
     }
-        public void OnPointerExit()
+
+    public void OnPointerExit()
     {
-        
+        GazeAt(false);
+    }
+
+    public void GazeAt(bool gazing)
+    {
+        if (gazing)
+        {
+            colorChaning = true;
+        }
+        else
+        {
+            myTimer = 0f;
+            colorChaning = false;
+        }
     }
 }
