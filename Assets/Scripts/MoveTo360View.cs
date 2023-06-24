b7bb9a0244646fe0ab0b42570244a568e5bbd044
ErOs <104714204+VarEros@users.noreply.github.com>
2023-06-23 22:08:05 -0600

    correcciones de estructura y en efecto de regreso

---
+++ Assets/Scripts/MoveTo360View.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class MoveTo360View : MonoBehaviour
+{
+    /// <summary>
+    /// The material to use when this object is inactive (not being gazed at).
+    /// </summary>
+    [SerializeField] private GameObject target;
+    [SerializeField] private GameObject sphere;
+    private MeshRenderer _myRenderer;
+    private AudioSource _mySound;
+    //private Vector3 teleportPose;
+
+    private bool colorChaning = false;
+    private float myTimer = 0f;
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        _myRenderer = GetComponent<MeshRenderer>();
+        _mySound = sphere.GetComponent<AudioSource>();
+        //teleportPose = new Vector3(sphere.transform.position.x, sphere.transform.position.y, sphere.transform.position.z)
+    }
+
+    public void Update()
+    {
+        if (colorChaning) {
+            myTimer += Time.deltaTime;
+            if (myTimer >= 2f)
+            {
+                target.transform.position = sphere.transform.position;
+                _mySound.Play();
+            }
+        }
+    }
+
+    public void OnPointerEnter()
+    {
+        GazeAt(true);
+    }
+
+    public void OnPointerExit()
+    {
+        GazeAt(false);
+    }
+
+    public void GazeAt(bool gazing)
+    {
+        if (gazing)
+        {
+            colorChaning = true;
+        }
+        else
+        {
+            myTimer = 0f;
+            colorChaning = false;
+        }
+    }
+}
